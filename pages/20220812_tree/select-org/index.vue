40879d03b7b062c09fa25e2e546243c60e9662fe
surviavalF <1278164813@qq.com>
2022-10-12 22:18:55 +0800

    提交未完成功能

--- pages/20220812_tree/select-org/index.vue
+++ pages/20220812_tree/select-org/index.vue
@@ -1,19 +1,71 @@
 <template>
-  <view class="org-select"> 
-
+  <view class="org-select">
+    <u-input
+      maxlength="99999999"
+      type="textarea"
+      :value="tips"
+      disabled
+      @click="onOpen"
+      :class="inputOpen ? '' : 'btnCheck'"
+    />
+    <u-popup v-model="popupShow" height="80%" @close="onClose" closeable>
+    </u-popup>
+    <u-toast ref="uToast" />
   </view>
 </template>
 
 <script>
+import _ from "lodash";
 export default {
+  props: {
+    value: {
+      //控制人员选择popup显隐
+      type: Boolean,
+      default: false
+    }
+  },
+  watch: {
+    value(val) {
+      this.popupShow = val;
+
+      if (val) {
+      } else {
+      }
+    },
+    checked: {
+      handler(val) {
+        const self = this;
+        if (!_.isEmpty(self.checked)) {
+          self.setUserNameList();
+        } else {
+          self.tips = "未选择";
+        }
+      },
+      deep: true,
+      imediate: true
+    }
+  },
   onLoad() {},
   onShow() {},
   components: {},
   data() {
-    return {};
+    return {
+      popupShow: false, //弹窗显隐
+      tips: "未选择"
+    };
   },
-  methods: {}
+  methods: {
+    //打开弹窗
+    onOpen() {
+      this.$emit("input", true);
+    },
+    //关闭弹窗
+    onClose() {
+      this.$emit("input", false);
+    }
+  }
 };
 </script>
 <style lang="scss" scoped>
+@import url("./css/icon.css");
 </style>
