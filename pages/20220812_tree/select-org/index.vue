b257270068862072557bf4db3bf25f52bdf3a10c
zhangyifeng <zhangyifeng@vrgvtech.com>
2022-10-12 18:55:30 +0800

    优化demo

---
+++ pages/20220812_tree/select-org/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <view class="org-select"> 
+
+  </view>
+</template>
+
+<script>
+export default {
+  onLoad() {},
+  onShow() {},
+  components: {},
+  data() {
+    return {};
+  },
+  methods: {}
+};
+</script>
+<style lang="scss" scoped>
+</style>
